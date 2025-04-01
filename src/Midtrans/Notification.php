c7566b8aef16a60ad13f70584807bcde4359779c
masraga <github.ragamulia@gmail.com>
2025-04-01 09:51:57 +0700

    Add midtrans notification

---
+++ src/Midtrans/Notification.php
@@ -0,0 +1,21 @@
+<?php
+
+use Koderpedia\LaravelBayarkan\Midtrans;
+
+class Notification
+{
+
+  public $response;
+
+  public function __construct($response = "php://input")
+  {
+    $this->response = json_decode(file_get_contents($response), true);
+  }
+
+  public function __get($name)
+  {
+    if ($this->response[$name]) {
+      return $this->response[$name];
+    }
+  }
+}
